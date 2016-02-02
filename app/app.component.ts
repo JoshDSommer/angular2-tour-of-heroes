63915ef6d4f36556f10820e7bbb51c9203167a7b
Josh Sommer <JoshDSommer@gmail.com>
2016-02-01 21:03:02 -0500

    part 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HEROES} from './mock-heroes';
+import {HeroService} from './hero.service';
+import {OnInit} from 'angular2/core';
+
 @Component({
-  selector: 'my-app',
-  template:`
+	selector: 'my-app',
+	template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -15,7 +19,7 @@ import {HeroDetailComponent} from './hero-detail.component';
     </ul>
     <my-hero-detail [hero]="selectedHero" [dog]="{'name':'cat'}"></my-hero-detail>
   `,
-  styles:[`
+	styles: [`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
@@ -63,23 +67,20 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0px 0px 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+	providers: [HeroService],
+	directives: [HeroDetailComponent]
 })
-export class AppComponent {
-  public title = 'Tour of Heroes';
-  public heroes = HEROES;
-  public selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
+export class AppComponent implements OnInit {
+	public title = 'Tour of Heroes';
+	public heroes: Hero[];
+	public selectedHero: Hero;
+	constructor(private _heroService: HeroService) { }
+	getHeroes() {
+		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+	ngOnInit() {
+		this.getHeroes();
+	}
+	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
+
